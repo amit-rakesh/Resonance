007a1a2afd5239562102b9591d4eeb0eae1d5e51
harkiratsingh <harkirat.singh.1990@gmail.com>
2016-04-01 16:52:38 -0700

    signup and login

--- src/main/java/edu/sjsu/config/RootConfig.java
+++ src/main/java/edu/sjsu/config/RootConfig.java
@@ -1,5 +1,7 @@
+
 package edu.sjsu.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +11,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import edu.sjsu.helpers.EmailNotification;
+
 @Configuration
 @ComponentScan(basePackages = { "edu.sjsu" }, excludeFilters = {
 		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
 @Import(JpaConfig.class)
 public class RootConfig {
+	
+    @Bean
+    public EmailNotification getEmailNotification(){
+        return new EmailNotification();
+    }
 
 }
--- src/main/java/edu/sjsu/models/UserDao.java
+++ src/main/java/edu/sjsu/models/UserDao.java
@@ -1,9 +1,15 @@
 package edu.sjsu.models;
 
+import java.util.ArrayList;
+import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 public interface UserDao extends CrudRepository<User, Long> {
+	
+    @Query(value="SELECT * FROM users u  WHERE  u.email=?1",nativeQuery = true)
+    User getUserByEmail(String email);
 
 }
