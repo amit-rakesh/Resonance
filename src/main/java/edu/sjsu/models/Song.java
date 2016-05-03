390e150e241d2e5171f63fd192251cae93860e5f
harkiratsingh <harkirat.singh.1990@gmail.com>
2016-05-03 14:39:45 -0700

    added a feild username to song

--- src/main/java/edu/sjsu/models/Song.java
+++ src/main/java/edu/sjsu/models/Song.java
@@ -34,6 +34,11 @@ public class Song {
     @Column(name = "userid")
     private Long uploadedByUserId;
     
+
+    @JsonProperty
+    @Column(name = "username")
+    private String uploadedByUserName;
+    
     @NotNull
     @JsonProperty
     @Column(name = "path")
@@ -74,6 +79,14 @@ public class Song {
         this.songPath=songPath;
         this.uploadedByUserId=uploadedByUserId;
         this.date=date;
+    }
+    
+    public Song(@JsonProperty String songTitle, @JsonProperty long uploadedByUserId,@JsonProperty String songPath, @JsonProperty long date, @JsonProperty String uploadedByUserName) {
+        this.songTitle = songTitle;
+        this.songPath=songPath;
+        this.uploadedByUserId=uploadedByUserId;
+        this.date=date;
+        this.uploadedByUserName = uploadedByUserName;
     }
 
     public long getDate() {
